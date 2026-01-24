ba08014ca1192cf1ded3b061eb32aa178534ee20
Anand Kumar <44093517+anand22121996@users.noreply.github.com>
2026-01-24 16:26:44 +0530

    dotfiles repo created

---
+++ private_dot_config/nvim/init.lua
@@ -0,0 +1,13 @@
+-- SETTINGS
+vim.g.mapleader = " "              -- Use space as leader key
+vim.opt.number = true               -- Show line numbers
+vim.opt.relativenumber = true       -- Relative line numbers for easier jumping
+vim.opt.mouse = ''                 -- Enable mouse support
+vim.opt.clipboard = "unnamedplus"   -- Sync with system clipboard
+vim.opt.ignorecase = true           -- Case-insensitive searching
+vim.opt.smartcase = true            -- Case-sensitive if search contains uppercase
+vim.opt.tabstop = 4                 -- Number of spaces for a tab
+vim.opt.shiftwidth = 4              -- Indentation width
+vim.opt.expandtab = true            -- Convert tabs to spaces
+vim.opt.termguicolors = true        -- Better color support for modern terminals
+vim.opt.undofile = true             -- Persistent undo history
